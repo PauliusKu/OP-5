738de8ba7d10bb83db8a464ae63c07e96599a716
PauliusKu <paulius.kuprevicius@gmail.com>
2018-05-30 20:59:26 +0300

    Galutinis

--- Header.h
+++ Header.h
@@ -10,11 +10,17 @@
 #include <vector>
 #include <sstream>
 
+inline int I() { return 5; }
+
+
+const bool b = true;
+
 typedef std::map<std::string, std::vector<unsigned int>> MapV;
 typedef std::pair<std::string, std::vector<unsigned int>>  PairV;
 typedef std::pair<MapV::iterator, bool> MapPairV;
 
 void Replace(std::string&, std::vector<char>&, const char&);
 void Spausdinti(MapV&);
+
 
 #endif
